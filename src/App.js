04ea1b23250333a49736f13eba67007551bd4fcc
Jevoni <jalenjay23@gmail.com>
2022-09-28 16:48:20 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from './components/Navbar'
+import Exchanges from './components/Exchanges'
+import Homepage from './components/Homepage'
+import Cryptocurrencies from './components/Cryptocurrencies'
+import News from './components/News'
+import CryptoDetails from './components/CryptoDetails'
+
+import './App.css'
+
+const App = () => {
+    return (
+        <div className='app'>
+            <div className='navbar'>
+                <Navbar />
+            </div>
+            <div className='main'>
+                <Layout>
+                    <div className='routes'>
+                        <Routes>
+                            <Route exact path='/' element={<Homepage />} />
+                            <Route exact path='/exchanges' element={<Exchanges />} />
+                            <Route exact path='/cryptocurrencies' element={<Cryptocurrencies />} />
+                            <Route exact path='/crypto/:coinId' element={<CryptoDetails />} />
+                            <Route exact path='/news' element={<News />} />
+                        </Routes>
+                    </div>
+                </Layout>
+                <div className='footer'>
+                    <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+                        Cryptoverse <br />
+                        All Rights reserved
+                    </Typography.Title>
+                    <Space>
+                        <Link to='/'>Home</Link>
+                        <Link to='/exchanges'>Exchange</Link>
+                        <Link to='/news'>News</Link>
+                    </Space>
+                </div>
+            </div>
+        </div >
+    )
 }
 
-export default App;
+export default App
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoNewsApiHeaders = {
+    'X-BingApis-SDK': 'true',
+    'X-RapidAPI-Key': '718d3fe800msha1bab973e1d8234p1596e7jsn0557e5ee49fa',
+    'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+}
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoNewsApiHeaders })
+
+export const cryptoNewsApi = createApi({
+    reducerPath: 'cryptoApiNews',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptoNews: builder.query({
+            query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`)
+        })
+    })
+})
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
